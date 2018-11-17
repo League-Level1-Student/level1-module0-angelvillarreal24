3455222308d80769698fbcd54cf9de9e79134f8b
angelvillarreal24 <league@Groovy.local>
2018-11-17 11:26:13 -0800

    Almost done with Photo quiz
    learned code 35

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,31 +22,37 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-                String x = "www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiKiMG_vcreAhUIDHwKHc8aBEoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.redbubble.com%2Fpeople%2Fmattysus%2Fworks%2F29999062-omegalul-twitch-emote%3Fp%3Dphotographic-print&psig=AOvVaw2yHcDwfvIFu4HvXTe8CBDI&ust=1541961798795111";
+                String x = "https://t5.rbxcdn.com/75fa5ba942d1ed749182425d14ea8401";
 		// 2. create a variable of type "Component" that will hold your image
                 Component c;
 		// 3. use the "createImage()" method below to initialize your Component
                 c = createImage(x);
 		// 4. add the image to the quiz window
-                
+                quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+                quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+                String ans = JOptionPane.showInputDialog("What is this twitch emote? hint: it may be Omega_LUL, Feelsbadman, or 4head.");
 		// 7. print "CORRECT" if the user gave the right answer
-
+                if(ans.equalsIgnoreCase("Omega_LUL")) {
+                	System.out.println("CORRECT!!!");
+                }
 		// 8. print "INCORRECT" if the answer is wrong
-
+                else {
+                	System.out.println("INCORRECT!");
+                }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+                quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line of code)
-
+                String y = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiJt6OtkdzeAhWrwVQKHZsaBLsQjRx6BAgBEAU&url=http%3A%2F%2Fwww.stickpng.com%2Fimg%2Fmemes%2Fpepe-the-frog%2Fsad-pepe-feelsbadman&psig=AOvVaw0UdtvoTVY7eY3kEK7HHJDG&ust=1542568366334904";
+                Component f;
+                f = createImage(y);
 		// 11. add the second image to the quiz window
-
+                
 		// 12. pack the quiz window
-
+                quizWindow.pack();
 		// 13. ask another question
-
+                String anz = JOptionPane.showInputDialog("What is this emote? hint: it may be feelsbadman, feelsgoodman, or 4head.");
 		// 14+ check answer, say if correct or incorrect, etc.
 
 	}
